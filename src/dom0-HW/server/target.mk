e2b12a188e605c6a4eaeadbb09bba9beec92ddaa
BugUser0815 <reisner.alex93@gmail.com>
2016-11-15 15:22:06 +0100

    Change directory due to include issues

--- src/dom0-HW/server/target.mk
+++ src/dom0-HW/server/target.mk
@@ -2,4 +2,4 @@ TARGET = dom0
 SRC_CC = main.cc tcp_socket.cc dom0_server.cc config.cc
 LIBS = base config lwip libc stdcxx
 
-INC_DIR += $(REP_DIR)/../libports/include/lwip
+INC_DIR += $(REP_DIR)/../genode/repos/libports/include/lwip
