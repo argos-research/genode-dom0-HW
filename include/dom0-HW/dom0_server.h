7d896d43e12a747b357b8e720a12ab1a45695562
BugUser0815 <ga67wel@mytum.de>
2018-07-02 11:33:27 +0200

    Remove child starter thread

--- include/dom0-HW/dom0_server.h
+++ include/dom0-HW/dom0_server.h
@@ -33,50 +33,32 @@ namespace Dom0_server{
 class Dom0_server// : public Tcp_socket
 {
 private:
-	class Child_starter_thread : Genode::Thread_deprecated<2*4096>
-	{
-	public:
-		Child_starter_thread();
-		void do_start(int target_socket);
-		void do_stop(int target_socket);
-		void do_clear(int target_socket);
-		void do_send_descs(int target_socket);
-		void do_send_binaries(int target_socket);
-		void do_send_profile(int target_socket);
-		void do_send_live(int target_socket);
-		ssize_t receive_data(void* data, size_t size, int _target_socket);
-		ssize_t receiveInt32_t(int32_t& data, int _target_socket);
-		ssize_t send_data(void* data, size_t size, int _target_socket);
-		ssize_t sendInt32_t(int32_t data, int _target_socket);
-		
+	Genode::Env &env;
 
-	private:
-		Timer::Connection _timer;
-		Taskloader_connection _task_loader;
-		Parser_connection _parser;
-		void entry() override;
-	};
+	int target_socket {};
+	struct sockaddr_in _target_sockaddr_in {};
 
-	int _target_socket;
-	struct sockaddr_in _target_sockaddr_in;
-
-	int _listen_socket;
-	struct sockaddr_in _in_addr;
-	sockaddr _target_addr;
-	Taskloader_connection _task_loader;
-	Parser_connection _parser;
+	int _listen_socket {};
+	struct sockaddr_in _in_addr {};
+	sockaddr _target_addr {};
+	Taskloader_connection _task_loader {};
+	Parser_connection _parser {};
 	Timer::Connection timer;
-	Sched_controller::Connection _controller;
-	static Child_starter_thread _starter_thread;
+	Sched_controller::Connection _controller {};
+
+	ssize_t receive_data(void* data, size_t size, int _target_socket);
+	ssize_t receiveInt32_t(int32_t& data, int _target_socket);
+	ssize_t send_data(void* data, size_t size, int _target_socket);
+	ssize_t sendInt32_t(int32_t data, int _target_socket);
 
 public:
-	Dom0_server();
+	Dom0_server(Genode::Env&);
 
 	~Dom0_server();
 
-	int connect();
+	int connect(Genode::Env&);
 
-	void serve();
+	void serve(Genode::Env&);
 
 	void disconnect();
 
--- src/dom0-HW/dom0_server.cc
+++ src/dom0-HW/dom0_server.cc
@@ -6,32 +6,32 @@
 
 #include <base/printf.h>
 #include <lwip/genode.h>
-#include <os/attached_ram_dataspace.h>
+#include <base/attached_ram_dataspace.h>
+#include <base/attached_rom_dataspace.h>
 #include <nic/packet_allocator.h>
 
 #include <dom0-HW/communication_magic_numbers.h>
 #include <timer_session/connection.h>
-#include <os/config.h>
+//#include <os/config.h>
 
-/* Fiasco includes */
+/* Fiasco includes
 namespace Fiasco {
 #include <l4/sys/kdebug.h>
-}
+}*/
 
 namespace Dom0_server {
 
-Dom0_server::Dom0_server() :
-	_listen_socket(0),
-	_in_addr{0},
-	_target_addr{0},
-	_task_loader{},
-	_parser{}
+Dom0_server::Dom0_server(Genode::Env &_env) :
+	env(_env),
+	timer(env)
 {
 	lwip_tcpip_init();
 
 	enum { BUF_SIZE = Nic::Packet_allocator::DEFAULT_PACKET_SIZE * 128 };
 
-	Genode::Xml_node network = Genode::config()->xml_node().sub_node("network");
+	Genode::Attached_rom_dataspace config(env, "config");
+
+	Genode::Xml_node network = config.xml().sub_node("network");
 
 	_in_addr.sin_family = AF_INET;
 
@@ -47,7 +47,7 @@ Dom0_server::Dom0_server() :
 	if (network.attribute_value<bool>("dhcp", true))
 	{
 		
-		PDBG("DHCP network...");
+		Genode::log("DHCP network...");
 		if (lwip_nic_init(0,
 		                  0,
 		                  0,
@@ -59,7 +59,7 @@ Dom0_server::Dom0_server() :
 	}
 	else
 	{
-		PDBG("manual network...");
+		Genode::log("manual network...");
 		char ip_addr[16] = {0};
 		char subnet[16] = {0};
 		char gateway[16] = {0};
@@ -93,7 +93,7 @@ Dom0_server::Dom0_server() :
 		PERR("Listen failed!");
 		return;
 	}
-	PINF("Listening...\n");
+	Genode::log("Listening...\n");
 }
 
 Dom0_server::~Dom0_server()
@@ -101,22 +101,22 @@ Dom0_server::~Dom0_server()
 	disconnect();
 }
 
-int Dom0_server::connect()
+int Dom0_server::connect(Genode::Env&)
 {
 	socklen_t len = sizeof(_target_addr);
-	_target_socket = lwip_accept(_listen_socket, &_target_addr, &len);
-	if (_target_socket < 0)
+	target_socket = lwip_accept(_listen_socket, &_target_addr, &len);
+	if (target_socket < 0)
 	{
 		PWRN("Invalid socket from accept!");
-		return _target_socket;
+		return target_socket;
 	}
-	sockaddr_in* target_in_addr = (sockaddr_in*)&_target_addr;
-	PINF("Got connection from %s", inet_ntoa(target_in_addr));
-	return _target_socket;
+	//sockaddr_in* target_in_addr = (sockaddr_in*)&_target_addr;
+	Genode::log("Got connection");
+	return target_socket;
 }
 
 // Receive data from the socket and write it into data.
-ssize_t Dom0_server::Child_starter_thread::receive_data(void* data, size_t size, int _target_socket)
+ssize_t Dom0_server::receive_data(void* data, size_t size, int _target_socket)
 {
 	ssize_t result = 0;
 	ssize_t position = 0;
@@ -138,13 +138,13 @@ ssize_t Dom0_server::Child_starter_thread::receive_data(void* data, size_t size,
 }
 
 // convenience function
-ssize_t Dom0_server::Child_starter_thread::receiveInt32_t(int32_t& data, int _target_socket)
+ssize_t Dom0_server::receiveInt32_t(int32_t& data, int _target_socket)
 {
 	return receive_data(&data, sizeof(data), _target_socket);
 }
 
 // Send data from buffer data with size size to the socket.
-ssize_t Dom0_server::Child_starter_thread::send_data(void* data, size_t size, int _target_socket)
+ssize_t Dom0_server::send_data(void* data, size_t size, int _target_socket)
 {
 	ssize_t result = 0;
 	ssize_t position = 0;
@@ -165,63 +165,111 @@ ssize_t Dom0_server::Child_starter_thread::send_data(void* data, size_t size, in
 }
 
 // convenience function
-ssize_t Dom0_server::Child_starter_thread::sendInt32_t(int32_t data, int _target_socket)
+ssize_t Dom0_server::sendInt32_t(int32_t data, int _target_socket)
 {
 	return send_data(&data, sizeof(data), _target_socket);
 }
 
-void Dom0_server::serve()
+void Dom0_server::serve(Genode::Env& env)
 {
 	int message = 0;
 	while (true)
 	{
-		NETCHECK_LOOP(_starter_thread.receiveInt32_t(message, _target_socket));
+		NETCHECK_LOOP(receiveInt32_t(message, target_socket));
 		if (message == SEND_DESCS)
 		{
-			int time_before=timer.elapsed_ms();
-			_starter_thread.do_send_descs(_target_socket);
-			PDBG("Done SEND_DESCS. Took: %d",timer.elapsed_ms()-time_before);
+			Genode::log("Ready to receive task description.");
+
+			// Get XML size.
+			int xml_size;
+			receiveInt32_t(xml_size, target_socket);
+			Genode::Attached_ram_dataspace xml_ds(env.ram(), env.rm(), xml_size);
+			Genode::log("Ready to receive XML of size %d.", xml_size);
+
+			// Get XML file.
+			receive_data(xml_ds.local_addr<char>(), xml_size,target_socket);
+			Genode::log("Received XML. Initializing tasks.");
+			_task_loader.add_tasks(xml_ds.cap());
 		}
 		else if (message == CLEAR)
 		{
 			int time_before=timer.elapsed_ms();
-			_starter_thread.do_clear(_target_socket);
-			PDBG("Done START. Took: %d",timer.elapsed_ms()-time_before);
+			_task_loader.clear_tasks();
+			Genode::log("Done CLEAR. Took: %d",timer.elapsed_ms()-time_before);
 		}
 		else if (message == SEND_BINARIES)
 		{
-			int time_before=timer.elapsed_ms();
-			_starter_thread.do_send_binaries(_target_socket);
-			PDBG("Done SEND_BINARIES. Took: %d",timer.elapsed_ms()-time_before);
+			Genode::log("Ready to receive binaries.");
+
+			// Get number of binaries to receive.
+			int num_binaries = 0;
+			receiveInt32_t(num_binaries, target_socket);
+			Genode::log("%d binar%s to be sent.", num_binaries, num_binaries == 1 ? "y" : "ies");
+
+			// Receive binaries.
+			for (int i = 0; i < num_binaries; i++)
+			{
+				// Client is waiting for ready signal.
+				sendInt32_t(GO_SEND, target_socket);
+				// Get binary name.
+				Genode::Attached_ram_dataspace name_ds(env.ram(), env.rm(), 16);
+				receive_data(name_ds.local_addr<char>(), 16, target_socket);
+				// Get binary size.
+				int32_t binary_size = 0;
+				receiveInt32_t(binary_size, target_socket);
+				// Get binary data.
+				Genode::Dataspace_capability binDsCap = _task_loader.binary_ds(name_ds.cap(), binary_size);
+				Genode::Region_map* rm = &env.rm();
+				char* bin = (char*)rm->attach(binDsCap);
+				receive_data(bin, binary_size, target_socket);
+				Genode::log("Got binary");
+				rm->detach(bin);
+			}
 		}
 		else if (message == GET_LIVE)
 		{
-			int time_before=timer.elapsed_ms();
-			_starter_thread.do_send_live(_target_socket);
-			PDBG("Done GET_LIVE. Took: %d",timer.elapsed_ms()-time_before);
+			Genode::Dataspace_capability xmlDsCap = _parser.live_data();
+			Genode::Region_map* rm = &env.rm();
+			char* xml = (char*)rm->attach(xmlDsCap);
+			if(std::strlen(xml)>0)
+			{
+				size_t size = std::strlen(xml) + 1;
+				Genode::log("Sending live data of size %d", size);
+				sendInt32_t(size, target_socket);
+				send_data(xml, size, target_socket);
+				rm->detach(xml);
+			}
 		}
 		else if (message == START)
 		{
 			int time_before=timer.elapsed_ms();
-			_starter_thread.do_start(_target_socket);
-			PDBG("Done START. Took: %d",timer.elapsed_ms()-time_before);
+			_task_loader.start();
+			Genode::log("Done START. Took: %d",timer.elapsed_ms()-time_before);
 		}
 		else if (message == STOP)
 		{
 			int time_before=timer.elapsed_ms();
-			_starter_thread.do_stop(_target_socket);
-			PDBG("Done STOP. Took: %d",timer.elapsed_ms()-time_before);
+			_task_loader.stop();
+			Genode::log("Done STOP. Took: %d",timer.elapsed_ms()-time_before);
 		}
 		else if (message == GET_PROFILE)
 		{
-			int time_before=timer.elapsed_ms();
-			_starter_thread.do_send_profile(_target_socket);
-			PDBG("Done GET_PROFILE. Took: %d",timer.elapsed_ms()-time_before);
+			Genode::Dataspace_capability xmlDsCap = _task_loader.profile_data();
+			Genode::Region_map* rm = &env.rm();
+			char* xml = (char*)rm->attach(xmlDsCap);
+			if(std::strlen(xml)>0)
+			{
+				size_t size = std::strlen(xml) + 1;
+				Genode::log("Sending profile data of size %d", size);
+				sendInt32_t(size, target_socket);
+				send_data(xml, size, target_socket);
+				rm->detach(xml);
+			}
 		}
 		else if(message == REBOOT)
 		{
-			using namespace Fiasco;
-                	enter_kdebug("*#^");
+			//using namespace Fiasco;
+                	//enter_kdebug("*#^");
 		}
 		else
 		{
@@ -232,126 +280,17 @@ void Dom0_server::serve()
 
 void Dom0_server::disconnect()
 {
-	lwip_close(_target_socket);
+	lwip_close(target_socket);
 	PERR("Target socket closed.");
 	lwip_close(_listen_socket);
 	PERR("Server socket closed.");
 }
 
-Dom0_server::Child_starter_thread::Child_starter_thread() :
-	Thread_deprecated{"child_starter"}
-{
-	start();
-}
-
-void Dom0_server::Child_starter_thread::do_start(int target_socket)
-{
-	PDBG("Starting tasks.");
-	_task_loader.start();
-}
-
-void Dom0_server::Child_starter_thread::do_stop(int target_socket)
-{
-	PDBG("Stopping tasks.");
-	_task_loader.stop();
-}
-
-void Dom0_server::Child_starter_thread::do_clear(int target_socket)
-{
-	PDBG("Clearing tasks.");
-	_task_loader.clear_tasks();
-}
-
-void Dom0_server::Child_starter_thread::do_send_descs(int target_socket)
-{
-	PDBG("Ready to receive task description.");
-
-	// Get XML size.
-	int xml_size;
-	receiveInt32_t(xml_size, target_socket);
-	Genode::Attached_ram_dataspace xml_ds(Genode::env()->ram_session(), xml_size);
-	PINF("Ready to receive XML of size %d.", xml_size);
-
-	// Get XML file.
-	receive_data(xml_ds.local_addr<char>(), xml_size,target_socket);
-	PDBG("Received XML. Initializing tasks.");
-	_task_loader.add_tasks(xml_ds.cap());
-}
-
-void Dom0_server::Child_starter_thread::do_send_binaries(int target_socket)
-{
-	PDBG("Ready to receive binaries.");
-
-	// Get number of binaries to receive.
-	int num_binaries = 0;
-	receiveInt32_t(num_binaries, target_socket);
-	PINF("%d binar%s to be sent.", num_binaries, num_binaries == 1 ? "y" : "ies");
-
-	// Receive binaries.
-	for (int i = 0; i < num_binaries; i++)
-	{
-		// Client is waiting for ready signal.
-		sendInt32_t(GO_SEND, target_socket);
-		// Get binary name.
-		Genode::Attached_ram_dataspace name_ds(Genode::env()->ram_session(), 16);
-		receive_data(name_ds.local_addr<char>(), 16, target_socket);
-		// Get binary size.
-		int32_t binary_size = 0;
-		receiveInt32_t(binary_size, target_socket);
-		// Get binary data.
-		Genode::Dataspace_capability binDsCap = _task_loader.binary_ds(name_ds.cap(), binary_size);
-		Genode::Region_map* rm = Genode::env()->rm_session();
-		char* bin = (char*)rm->attach(binDsCap);
-		receive_data(bin, binary_size, target_socket);
-		PINF("Got binary '%s' of size %d.", name_ds.local_addr<char>(), binary_size);
-		rm->detach(bin);
-	}
-}
-
-void Dom0_server::Child_starter_thread::do_send_profile(int target_socket)
-{
-	Genode::Dataspace_capability xmlDsCap = _task_loader.profile_data();
-	Genode::Region_map* rm = Genode::env()->rm_session();
-	char* xml = (char*)rm->attach(xmlDsCap);
-	if(std::strlen(xml)>0)
-	{
-		size_t size = std::strlen(xml) + 1;
-		PINF("Sending profile data of size %d", size);
-		sendInt32_t(size, target_socket);
-		send_data(xml, size, target_socket);
-		rm->detach(xml);
-	}
-}
-
-void Dom0_server::Child_starter_thread::do_send_live(int target_socket)
-{
-	Genode::Dataspace_capability xmlDsCap = _parser.live_data();
-	Genode::Region_map* rm = Genode::env()->rm_session();
-	char* xml = (char*)rm->attach(xmlDsCap);
-	if(std::strlen(xml)>0)
-	{
-		size_t size = std::strlen(xml) + 1;
-		PINF("Sending live data of size %d", size);
-		sendInt32_t(size, target_socket);
-		send_data(xml, size, target_socket);
-		rm->detach(xml);
-	}
-}
-
-void Dom0_server::Child_starter_thread::entry()
-{
-	while (true)
-	{
-	}
-}
-
-
 
 void Dom0_server::send_profile(Genode::String<32> task_name)
 {
 	_controller.optimize(task_name);
-	_starter_thread.do_send_profile(_target_socket);	
+	//_starter_thread.do_send_profile(_target_socket);	
 }
 
-Dom0_server::Child_starter_thread Dom0_server::_starter_thread;
 }
